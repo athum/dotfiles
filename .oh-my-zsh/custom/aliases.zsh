a275694f0107c154481dc365f2773490d970c90c
Alice Thum <ayhthum@gmail.com>
2026-01-04 17:59:19 -0500

    Add aliases.zsh

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,7 @@
+# aliases
+alias x='exit'
+
+alias zshcfg='$EDITOR $HOME/.zshrc'
+alias nvimcfg='$EDITOR $HOME/.config/nvim/init.vim'
+alias zshsrc='source $HOME/.zshrc'
+alias nvimsrc='source $HOME/.config/nvim/init.vim'
